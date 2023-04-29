938f504e1786137a0ff401819ece3e3227a43da0
suchethaleonidou <Suchethao>
2023-04-29 12:28:32 -0400

    added some comments explaining logic in todos.vue

--- mini-app/src/components/Todos.vue
+++ mini-app/src/components/Todos.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
+    <!-- Display the heading of the list -->
     <h2>Today's List</h2>
+    
+    <!-- Display the list of tasks if there are any -->
     <ul v-if="filteredTasks.length > 0">
       <li v-for="(task, index) in filteredTasks" :key="index" :class="{ completed: task.completed }">
         <span>{{ task.text }}</span>
         <button @click="completeTask(index)">Complete</button>
       </li>
     </ul>
+    
+    <!-- Display a message if there are no tasks in the list -->
     <p v-else> Add a task to your CheckMate list to get started. </p>
+    
+    <!-- Form to add new tasks to the list -->
     <form @submit.prevent="addTask">
       <label for="newTask">Add new task:</label>
       <input type="text" id="newTask" v-model="newTaskText">
@@ -19,6 +26,8 @@
 <script>
 export default {
   name: "AppTodos",
+  
+  // Set up the initial data of the component
   data() {
     return {
       tasks: [
@@ -29,6 +38,8 @@ export default {
       newTaskText: "",
     };
   },
+  
+  // Define the methods used in the component
   methods: {
     addTask() {
       if (this.newTaskText.trim() !== "") {
@@ -40,6 +51,8 @@ export default {
       this.tasks[index].completed = true;
     },
   },
+  
+  // Compute the list of tasks that still need to be completed
   computed: {
     filteredTasks() {
       return this.tasks.filter((task) => !task.completed);
@@ -47,6 +60,7 @@ export default {
   },
 };
 </script>
+
 <style scoped>
 .completed {
   text-decoration: line-through;
