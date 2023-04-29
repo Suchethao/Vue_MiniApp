ca11e4b2a225d98f1466143d7e29f36e3e86566a
suchethaleonidou <Suchethao>
2023-04-29 12:13:32 -0400

    added some comments on the header component

--- mini-app/src/components/Header.vue
+++ mini-app/src/components/Header.vue
@@ -1,3 +1,17 @@
+<!--
+This is the header component of the CheckMate app.
+It consists of a logo, search bar, and user actions.
+The logo is displayed in a separate div with the class "logo-container",
+while the search bar is in "search-container".
+The user actions, which are represented by icons, are displayed in "actions-container".
+
+The CSS for this component uses flexbox to arrange the elements.
+The header has a white background with a border at the bottom.
+The logo is styled to be 3rem high.
+The search bar has a light gray background with a border and rounded corners.
+The user action icons have a font size of 1.5rem and a margin of 1rem.
+-->
+
 <template>
   <div class="header-container">
     <div class="logo-container">
